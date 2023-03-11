54414805e98174413e6e4d301825043abfccfa6f
Mubashir Ahmed <mubashirahmed324@gmail.com>
2023-03-11 13:34:11 +0500

    project05_word_counter

---
+++ project05_word_counter/index.ts
@@ -0,0 +1,75 @@
+#!usr/bin/env node
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+import chalkanimation from "chalk-animation";
+
+//chalkanimation.neon('Welcome to CLI Calculator');
+
+const sleep = ()=> {
+    return new Promise((resolve) => {
+        setTimeout(resolve, 1500);
+    })
+}
+
+async function Welcome() {
+let rainbow = chalkanimation.rainbow('Welcome to Word Counter'); // Animation starts
+await sleep();
+rainbow.stop();
+//console.log(chalk.yellowBright(chalk.bold("Developed By Mubashir Ahmed \n")));
+}
+
+
+async function askQuestion(){
+    await inquirer
+  .prompt([
+
+    {
+      type:"input",
+      name:"word",
+      message:"Enter Word / sentence: "
+    }
+  ])
+  .then((answers) => {
+    let user_input:string = answers.word;
+    let wordlength = wordcount(user_input);
+  })
+
+}
+
+async function wordcount(word:string) {
+  let wordArray:string[] = word.split(' ');
+  let length:number = wordArray.length;
+  console.log(chalk.blueBright(`\nWord Count is ${chalk.yellowBright(length)}`));
+
+  let charactercount:number = 0;
+  for (let i = 0; i < wordArray.length; i++) {
+    charactercount = charactercount + wordArray[i].length;
+  }
+
+  console.log(chalk.blueBright(`Character Count is ${chalk.yellowBright(charactercount)}`));
+}
+
+
+async function startAgain () {
+    //console.log("Comp_choice = " + Comp_choice);
+    await Welcome();
+    
+   do{
+    console.clear();
+    await askQuestion();
+
+    var again = await inquirer
+  .prompt([
+    {
+        type:"input",
+        name:"restart",
+        message:"Do you want to run again? Press Y or N "
+    }
+  ])
+   } while(again.restart == "y" || again.restart == "Y" || again.restart == "yes")
+    
+   console.log(chalk.yellowBright(chalk.bold("Developed By Mubashir Ahmed \n")));
+}
+
+startAgain();
